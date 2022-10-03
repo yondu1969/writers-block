e18e502d06429a6fd2239e44826a77b17bccafa3
David Hang <dhang00@Davids-iMac.lan>
2022-10-02 21:52:01 -0700

    Using enlite prime

---
+++ application/fe/app/api/dummy/brand.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Enlite Starter',
+  desc: 'Enlite Starter - React.js Fullstack Template',
+  prefix: 'enlite',
+  footerText: 'Enlite Starter All Rights Reserved 2020',
+  logoText: 'Enlite Starter',
+};
---
+++ application/fe/app/utils/sagas.js
@@ -0,0 +1,8 @@
+import { all } from 'redux-saga/effects';
+import authSagas from 'enl-redux/modules/authSagas';
+
+export default function* sagas() {
+  yield all([
+    ...authSagas,
+  ]);
+}
